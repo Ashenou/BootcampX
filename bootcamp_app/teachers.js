d93f14709722a57efaca40c83ac4dbe0439845c6
Abanoub Shenouda <Abanoub Shenouda>
2021-06-23 22:10:52 -0400

    update teachers and students to use Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -16,20 +16,15 @@ pool
     console.log("-------- Error -------");
     console.log(e);
   });
-
-pool
-  .query(
-    `SELECT teachers.name as teacher, cohorts.NAME as cohort
+const values = [`%${process.argv[2]}%`];
+const querystring = `SELECT teachers.name as teacher, cohorts.NAME as cohort
   FROM teachers
   JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
   JOIN students ON students.id = assistance_requests.student_id
   JOIN cohorts ON students.cohort_id = cohorts.id
   GROUP BY teachers.name,cohorts.NAME
-  HAVING cohorts.name = 'JUL02';
-  `
-  )
-  .then((res) => {
-    res.rows.forEach((query) =>
-      console.log(`${query.cohort}: ${query.teacher}`)
-    );
-  });
+  HAVING cohorts.name Like $1`;
+
+pool.query(querystring, values).then((res) => {
+  res.rows.forEach((query) => console.log(`${query.cohort}: ${query.teacher}`));
+});
